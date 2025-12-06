efab2e55a5880c0e4cb310588a83250ed362bcb8
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2025-12-06 18:14:49 +0900

    refactor: update repository references and image names to mumeinosato namespace

--- pkg/controller/weel_known_annotations.go
+++ pkg/controller/weel_known_annotations.go
@@ -1,18 +1,18 @@
 package controller
 
 // AnnotationProxySSLVerify is the annotation key for proxy-ssl-verify, available values: "on" or "off", default "off".
-const AnnotationProxySSLVerify = "cloudflare-tunnel-ingress-controller.strrl.dev/proxy-ssl-verify"
+const AnnotationProxySSLVerify = "cloudflared-ingress.mumeinosato.github.io/proxy-ssl-verify"
 const AnnotationProxySSLVerifyOn = "on"
 const AnnotationProxySSLVerifyOff = "off"
 
 // AnnotationBackendProtocol is the annotation key for proxy-backend-protocol, default "http".
-const AnnotationBackendProtocol = "cloudflare-tunnel-ingress-controller.strrl.dev/backend-protocol"
+const AnnotationBackendProtocol = "cloudflared-ingress.mumeinosato.github.io/backend-protocol"
 
 // AnnotationHTTPHostHeader is to set the HTTP Host header for the local webserver.
-const AnnotationHTTPHostHeader = "cloudflare-tunnel-ingress-controller.strrl.dev/http-host-header"
+const AnnotationHTTPHostHeader = "cloudflared-ingress.mumeinosato.github.io/http-host-header"
 
 // AnnotationOriginServerName is the hostname on the origin server certificate.
-const AnnotationOriginServerName = "cloudflare-tunnel-ingress-controller.strrl.dev/origin-server-name"
+const AnnotationOriginServerName = "cloudflared-ingress.mumeinosato.github.io/origin-server-name"
 
 // CustomAnnotations
-const AnnotationDisableChunkedEncoding = "cloudflare-tunnel-ingress-controller.strrl.dev/disable-chunked-encoding"
+const AnnotationDisableChunkedEncoding = "cloudflared-ingress.mumeinosato.github.io/disable-chunked-encoding"
